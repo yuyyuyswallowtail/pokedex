1062177b0ae2c3fd3308012ee511d3cdf662ef64
Bintang Mesir <bintangmsr@gmail.com>
2026-03-03 04:57:04 +0700

    refsctor:Removed the unused MeshDistortMaterial import:

--- components/three/pokeball-3d.tsx
+++ components/three/pokeball-3d.tsx
@@ -2,7 +2,7 @@
 
 import { useRef, useMemo } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
-import { OrbitControls, Float, MeshDistortMaterial } from '@react-three/drei';
+import { OrbitControls, Float } from '@react-three/drei';
 import * as THREE from 'three';
 import { motion } from 'framer-motion';
 
